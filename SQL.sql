bd13c191ce1b580abb513d8f4951225881c215ec
me <timogoosen>
2017-03-12 09:31:31 +0200

    added queries

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,25 @@
-select timestamp, price, volume, total_transaction_cost from temptable;
+Select All Stuff:
+
+```
+
+select id, timestamp, price, volume, total_transaction_cost from temptable;
+
+
+```
+
+Select transaction with largest total_transaction_cost:
+(Does not mean this was largest transaction in terms of volume!!!)
+```
+
+select max(total_transaction_cost), id, timestamp from temptable;
+
+```
+
+Select transaction with largest volume:
+
+```
+
+select max(volume), id, timestamp from temptable;
+
+
+```
